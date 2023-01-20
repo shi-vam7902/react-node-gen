104ff5f0a415f5e5595955a307589e18af005d58
shi-vam7902 <shivashah.glsbca20@gmail.com>
2023-01-20 22:00:09 +0530

    MailerService

--- middleware/AuthMiddleWare.js
+++ middleware/AuthMiddleWare.js
@@ -1,17 +1,24 @@
-const Auth = (obj) => async (req,res,next)=>{
-    try{
-        if(obj === "ADMIN"){
-            return next()
-        }
-        else
-        {
-            throw new Error("This is UnAuthorised");
-        }
+const Auth = () => async (req, res, next) => {
+  try {
+    if (!req.headers.role) {
+        console.log("....",req.headers.role);
+      throw new Error("role is Missing");
+    } else {
+      // console.log(".",req.body);
+      console.log(req.headers.role);
+      const role = req.headers.role;
+
+      if (role === "ADMIN" || role === "admin" || role === "user") {
+        return next();
+      } else {
+        throw new Error("NO ACCESS");
+      }
     }
-    catch(err){
-        return res.status(401).json({
-            message:err.message
-        })
-    }
-}
-module.exports = {Auth}
+  } catch (err) {
+    return res.status(401).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { Auth };
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router= express.Router()
 const userController = require('../controller/UserController')
-const auth = require('../middleware/AuthMiddleWare')
+const Auth = require('../middleware/AuthMiddleWare')
 
 router.post("/user",userController.createUser)
-router.get("/user/auth",auth.Auth("ADMIN"),userController.getAllUser)
+router.get("/user",Auth.Auth(),userController.getAllUsers)
 // router.route('/').get('/auth', auth.Auth("ADMIN"));
 router.delete("/user/:id",userController.deleteUser)
 router.put("/user/:id",userController.updateUser)
